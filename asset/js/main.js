912dfe24c75a7e9f38f53f0a02118de789950186
olawale-oloye <olawaleoloye@gmail.com>
2023-07-07 12:58:38 +1000

    updated responsive pages, eliminated css errors and implemented JS for responsiveness

--- asset/js/main.js
+++ asset/js/main.js
@@ -17,3 +17,27 @@ arrow.addEventListener("click", function () {
   document.documentElement.scrollTop = 0;
   document.body.scrollTop = 0;
 });
+
+const openNav = document.querySelector(".ri-menu-2-fill");
+const closeNav = document.querySelector(".ri-close-circle-line");
+const navList = document.querySelector("nav ul");
+
+openNav.addEventListener("click", function () {
+  navList.style.top = "30%";
+  openNav.style.display = "none";
+  closeNav.style.style.display = "block";
+});
+
+closeNav.addEventListener("click", function () {
+  navList.style.top = "-50%";
+  openNav.style.display = "block";
+  closeNav.style.display = "none";
+});
+
+document.querySelectorAll("nav ul a").forEach((navLink) => {
+  navLink.addEventListener("click", function () {
+    navList.style.top = "-50%";
+    openNav.style.display = "block";
+    closeNav.style.display = "none";
+  });
+});
